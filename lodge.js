7b7dce9ef195c7c4241e3d2a5accd94ece9ccf15
Kord Campbell <kordless@gmail.com>
2010-09-12 21:11:14 -0700

    let there be logs

---
+++ lodge.js
@@ -0,0 +1,137 @@
+// includes
+var sys = require("sys"),
+    http = require("http"),
+    net = require("net"),
+    url = require("url");
+
+// network objects
+var syslogclient;
+
+// log to local console 
+var log = function(content) {
+    sys.log(content);
+};
+
+// events handling
+var EventEmitter = require('events').EventEmitter;
+var syslogstart = new EventEmitter();
+syslogstart.addListener('start', function() {
+    if (syslogclient === undefined || syslogclient.readyState != "open") { 
+       syslogclient = net.createConnection(514, host='127.0.0.1');
+       log("notice: starting connection to syslog server");
+    } else {
+       log("notice: syslog server connection already established");
+    }
+
+    syslogclient.setNoDelay(noDelay=true);
+});
+
+// open the tcp connection to the syslog server
+syslogstart.emit('start');
+
+process.addListener("uncaughtException", function (err) {
+    log("error: caught an exception - " + err);
+    if (err.name === "AssertionError") throw err;
+    if (++exception_count == 4) process.exit(0);
+});
+
+// write event to syslog server 
+var forward_event = function(eventstamp, remoteip, content) {
+    // craft header
+    var header = eventstamp+" "+remoteip+" "+content+"\n";
+    if (syslogclient.readyState === "open") {
+	    // log("writing to syslog server: " + header.slice(0,80) + "...");
+        syslogclient.write(header);
+    } else {
+        // try to restart the connection, but tell the client to go away
+	log("notice: issuing syslog server restart");
+        syslogstart.emit('start');
+        return -1;
+    }
+};
+
+// key hash store
+var hashes = {}
+hashes['d00dadc0ffee'] = {'valid': 'true'};
+
+// server object
+lodge = http.createServer(function (request, response) {
+    var content = "";
+    var remoteip = request.connection.remoteAddress;
+
+    // getting some data
+    request.addListener("data", function(chunk) {
+        content += chunk;
+        if (content.length > 32768) {
+            response.writeHead(413, {"Content-Type": "application/json"});
+            response.write("{ 'response': 'false', 'message': 'error: oversized event' }\n");
+            response.end();
+            return;
+        }
+    });
+    request.addListener("end", function() {
+        var keyrequest;
+
+        // check path
+        pathname = url.parse(request.url, true).pathname; 
+        if (pathname.match('/inputs/[a-zA-Z0-9-]+/?') === null) {
+            response.writeHead(404, {"Content-Type": "application/json"});
+            response.write("{ 'response': 'false', 'message': 'error: method not found' }\n");
+            response.end();
+            return;
+        }
+        // check for key
+        key = pathname.split('/')[2]; 
+        if (key === undefined || key === null || key == '') { 
+            response.writeHead(400, {"Content-Type": "application/json"});
+            response.write("{ 'response': 'false', 'message': 'error: no input key provided' }\n");
+            response.end();
+            return;
+        } else {
+            // check cache for key
+            var cachekey = false;
+            var valid = false;
+
+            if (hashes[key] === undefined) {
+                cachekey = false;
+            } else {
+                cachekey = true;
+            }
+
+            if (cachekey == false) {
+                // key not valid 
+                if (valid === null || valid === false) {
+                    response.writeHead(400, {"Content-Type": "application/json"});
+                    response.write("{ 'response': 'false', 'message': 'error: key not validated' }\n");
+                    response.end();
+		    return;
+                } 
+            } else {
+                // craft a date syslog will recognize  
+                var dt = new Date();
+                var hours = dt.getHours();
+                var minutes = dt.getMinutes();
+                var seconds = dt.getSeconds();
+                var month = dt.getMonth();
+                var day = dt.getDate();
+                var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                var eventstamp = months[month] + " " + day + " " + hours + ":" + minutes + ":" + seconds;
+                fc = forward_event(eventstamp, remoteip, content);
+
+                // if event wasn't forwarded, warn
+                if (fc == -1) {
+                    response.writeHead(500, {"Content-Type": "application/json"});
+                    response.write("{ 'response': 'false', 'message': 'error: try again later' }\n");
+                    response.end();
+		    return;
+                 }
+
+                // notify user
+                response.writeHead(201, {"Content-Type": "application/json"});
+                response.write("{ 'response': 'true', 'message': 'info: event received', 'eventstamp': "+eventstamp+"  }\n");
+                response.end();
+                return;
+            }
+        }
+    });
+}).listen(8080);
---
+++ sample.js
@@ -0,0 +1,23 @@
+// includes
+var sys = require("sys"),
+    http = require("http");
+
+// connection to the lodge server
+var localhost = http.createClient(8080, host='localhost');
+
+// log to local console and lodge
+log = function(content) {
+    sys.log(content);
+    localpost = localhost.request('POST', '/inputs/d00dadc0ffee', {'host': 'localhost'});
+    localpost.write(content);
+    localpost.end();
+};
+
+// simple server object
+sample = http.createServer(function (request, response) {
+    var remoteip = request.connection.remoteAddress;
+    // on connection end, log an access-combined like format
+    response.writeHead(200, {"Content-Type": "text/html"});
+	response.end("howdy partner");
+	log(remoteip + ' "GET ' + request.url + '" "' + request.headers['user-agent'] + '"');
+}).listen(8081);
