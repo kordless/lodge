a17947b5a57d177174fd77cd04d10a8bfc213c37
Kord Campbell <kordless@gmail.com>
2010-09-12 22:42:49 -0700

    spaces

--- sample.js
+++ sample.js
@@ -18,6 +18,6 @@ sample = http.createServer(function (request, response) {
     var remoteip = request.connection.remoteAddress;
     // on connection end, log an access-combined like format
     response.writeHead(200, {"Content-Type": "text/html"});
-	response.end("howdy partner");
-	log(remoteip + ' "GET ' + request.url + '" "' + request.headers['user-agent'] + '"');
+    response.end("howdy partner");
+    log(remoteip + ' "GET ' + request.url + '" "' + request.headers['user-agent'] + '"');
 }).listen(8081);
